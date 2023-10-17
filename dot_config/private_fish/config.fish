7b1e8875566b45b291f4199977d39eea385f7e53
Alexander Stensrud <alex@yuki.no>
2023-10-17 05:26:58 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
   set --global fish_pager_color_selected_background --background=4b454b
   set --global fish_greeting
 
+  function install-lexical
+    rm -rf ~/.local/share/lexical && mix package --path ~/.local/share/lexical
+  end
+
   function lk
     set loc (walk $argv); and cd $loc;
   end
@@ -14,12 +18,13 @@ if status is-interactive
   fish_add_path $HOME/.ghcup/bin
   fish_add_path $HOME/.cargo/bin
   fish_add_path $(go env GOPATH)/bin
-  fish_add_path /opt/homebrew/opt/ruby/bin 
-  fish_add_path /opt/homebrew/lib/ruby/gems/3.2.0/bin/
-  fish_add_path /opt/homebrew/opt/postgresql@15/bin
-  fish_add_path /opt/homebrew/opt/node@18/bin
-  fish_add_path /opt/homebrew/opt/make/libexec/gnubin
-  fish_add_path $HOME/.local/share/lexical/bin
+  fish_add_path --prepend /opt/homebrew/opt/ruby/bin 
+  fish_add_path --prepend /opt/homebrew/lib/ruby/gems/3.2.0/bin/
+  fish_add_path --prepend /opt/homebrew/opt/postgresql@15/bin
+  fish_add_path --prepend /opt/homebrew/opt/node@18/bin
+  fish_add_path --prepend /opt/homebrew/opt/make/libexec/gnubin
+  fish_add_path --prepend /opt/homebrew/opt/gnu-tar/libexec/gnubin
+  fish_add_path --prepend $HOME/.local/share/lexical/bin
   fish_add_path --prepend $HOME/.local/bin --move
 
   set --export EDITOR hx
@@ -29,12 +34,16 @@ if status is-interactive
   set --export XDG_CONFIG_HOME $HOME/.config
   set --export HOMEBREW_NO_ANALYTICS 1
 
-  alias gf 'gum filter --prompt="# " --indicator="+" --placeholder=""'
-  alias k kubecolor
-  alias mv 'mv -v'
-  alias cp 'cp -v'
+  set --export NNN_COLORS '#50b0eeee;1234'
+  set --export NNN_FCOLORS '000000000000000000000000'
+
+  alias gf='gum filter --prompt="# " --indicator="+" --placeholder=""'
+  alias k=kubecolor
+  alias mv='mv -v'
+  alias cp='cp -v'
   alias tailscale='/Applications/Tailscale.app/Contents/MacOS/Tailscale'
   alias gl='git log --graph --pretty=oneline --abbrev-commit'
+  alias d='nnn -eAdHR'
 
   #source ~/.asdf/asdf.fish
   rtx activate fish | source
